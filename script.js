99582925778e0cea1d5fb168fc45e0eb6b87dcd8
eliptik <adamfeyk5@gmail.com>
2022-10-16 15:45:24 +0300

    Limit the grid size to 100

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ btn.addEventListener("click", removePad)
 
 
 
-let row = 5;
+let row = 16;
 let column = row;
 createPad();
 console.log(row)
@@ -18,13 +18,19 @@ function removePad() {
 }
 
 function createPad() {
+    
+    while(row >= 100){
+        alert("You should set the grid size smaller than 100")
+        row = prompt("Set the grid size");
+        column = row
+    }
 
     for (let j = 1; j <= column; j++) {
         for (let i = 1; i <= row; i++) {
             const pixel2 = document.createElement("div")
             pixel2.classList.add("pixel")
-            pixel2.style.width = `${400 / row}px`
-            pixel2.style.height = `${400 / row}px`
+            pixel2.style.width = `${800 / row}px`
+            pixel2.style.height = `${800 / row}px`
             container.append(pixel2)
         }
     }
