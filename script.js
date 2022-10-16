893b37463ab51761b4b3d4e37614ba45da136db6
eliptik <adamfeyk5@gmail.com>
2022-10-16 15:15:17 +0300

    Create removePad function & grid size option

--- script.js
+++ script.js
@@ -1,32 +1,40 @@
 const container = document.querySelector(".container")
+const btn = document.querySelector("#btn")
+const btn2 = document.querySelector("#btn2")
+btn.addEventListener("click", removePad)
 
-let row = prompt();
+
+
+let row = 5;
 let column = row;
-
 createPad();
+console.log(row)
+function removePad() {
+    const cont = document.querySelector(".container")
+    cont.innerHTML = ""
+    row = prompt("Set the grid size");
+    column = row;
+    createPad();
+}
 
-function createPad(){
-for (let j = 1; j <= column; j++) {
-    for (let i = 1; i <= row; i++) {
-        const pixel2 = document.createElement("div")
-        pixel2.classList.add("pixel")
-        pixel2.style.width = `${400/row}px`
-        pixel2.style.height = `${400/row}px`
-        container.append(pixel2)
+function createPad() {
+
+    for (let j = 1; j <= column; j++) {
+        for (let i = 1; i <= row; i++) {
+            const pixel2 = document.createElement("div")
+            pixel2.classList.add("pixel")
+            pixel2.style.width = `${400 / row}px`
+            pixel2.style.height = `${400 / row}px`
+            container.append(pixel2)
+        }
+    }
+
+    const selection = document.getElementsByClassName("pixel")
+    for (let i = 0; i < selection.length; i++) {
+        selection[i].addEventListener("mouseover", hover)
+
+        function hover() {
+            selection[i].classList.add("color")
+        }
     }
 }
-}
-
-const selection = document.getElementsByClassName("pixel")
-for (let i = 0; i < selection.length; i++) {
-    selection[i].addEventListener("mouseover", hover)
-
-    function hover() {
-        selection[i].classList.add("color")
-    }
-}
-
-
-
-
-
